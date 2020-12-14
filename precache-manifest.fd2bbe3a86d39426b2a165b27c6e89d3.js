40dc4c431696672915133b5a27b220feadf4d6aa
mathieu raimbault <mathieu.raimbault@hotmail.com>
2020-12-14 01:12:52 +0100

    Updates

---
+++ precache-manifest.fd2bbe3a86d39426b2a165b27c6e89d3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a941d9bd9d18fbcaae0ef9183e30f01c",
+    "url": "/kitten-integration/index.html"
+  },
+  {
+    "revision": "e905367e7ca685c2b2e5",
+    "url": "/kitten-integration/static/css/main.1f7d67d7.chunk.css"
+  },
+  {
+    "revision": "da5cfcefb2d2eb165d28",
+    "url": "/kitten-integration/static/js/2.d5a0e956.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/kitten-integration/static/js/2.d5a0e956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e905367e7ca685c2b2e5",
+    "url": "/kitten-integration/static/js/main.72940773.chunk.js"
+  },
+  {
+    "revision": "62118e3ccae973c4da96",
+    "url": "/kitten-integration/static/js/runtime-main.54e48f9d.js"
+  },
+  {
+    "revision": "82173891a8c7fcab744821265d4773d2",
+    "url": "/kitten-integration/static/media/Didot-medium.82173891.ttf"
+  },
+  {
+    "revision": "b9ab4ff51393c5e20db6207e07a0711b",
+    "url": "/kitten-integration/static/media/Moon-Bold.b9ab4ff5.otf"
+  },
+  {
+    "revision": "14e8e211ca6c8cd2bc46f566ab2a3c67",
+    "url": "/kitten-integration/static/media/Moon2.0-Light.14e8e211.otf"
+  },
+  {
+    "revision": "beaab26664137dc78edd6a735b31b40d",
+    "url": "/kitten-integration/static/media/Moon2.0-Regular.beaab266.otf"
+  },
+  {
+    "revision": "f1aec09f6349b04a9af51b987512fea9",
+    "url": "/kitten-integration/static/media/img1.f1aec09f.png"
+  },
+  {
+    "revision": "856a70d4eb6cf0ee1ea453f13a936283",
+    "url": "/kitten-integration/static/media/img2.856a70d4.png"
+  },
+  {
+    "revision": "9348123f05691895508027581e9c9eb3",
+    "url": "/kitten-integration/static/media/img3.9348123f.png"
+  }
+]);
